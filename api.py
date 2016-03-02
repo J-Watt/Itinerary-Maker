2c5a86a51034e50e5c5c37b3d4ef481f3f3dda6c
Jordan A Watt <jordanalexwatt@hotmail.com>
2016-03-02 16:34:21 +0000

    Add basic API endpoints for creating/updating

--- api.py
+++ api.py
@@ -1,16 +1,148 @@
 #-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
+# Name:        api.py
+# Purpose:     Itinerary Maker API server exposing resources
 #
-# Author:      Jordan
+# Author:      Jordan Alexander Watt
 #
 # Created:     29-02-2016
 # Copyright:   (c) Jordan 2016
-# Licence:     <your licence>
 #-------------------------------------------------------------------------------
 
-def main():
-    pass
+import logging
+import endpoints
 
-if __name__ == '__main__':
-    main()
+from protorpc import remote
+from protorpc import messages
+from protorpc import message_types
+from google.appengine.api import memcache
+from google.appengine.ext import ndb
+from google.appengine.api import taskqueue
+
+from models import User
+from models import Itinerary
+from models import TransportItem
+from models import Checklist
+from models import ListItem
+from models import UserForm
+from models import ItineraryForm
+from models import TransportForm
+from models import ChecklistForm
+from models import ListItemForm
+
+from settings import WEB_CLIENT_ID
+from settings import ANDROID_CLIENT_ID
+from settings import IOS_CLIENT_ID
+from settings import ANDROID_AUDIENCE
+
+API_EXPLORER_CLIENT_ID = endpoints.API_EXPLORER_CLIENT_ID
+
+UPDATE_USER_REQUEST = endpoints.ResourceContainer(UserForm)
+UPDATE_LIST_REQUEST = endpoints.ResourceContainer(
+    ChecklistForm,
+    urlsafe_list_key=messages.StringField(1))
+UPDATE_ITIN_REQUEST = endpoints.ResourceContainer(
+    ItineraryForm,
+    urlsafe_itin_key=messages.StringField(1))
+UPDATE_TRAN_REQUEST = endpoints.ResourceContainer(
+    TransportForm,
+    urlsafe_tran_key=messages.StringField(1))
+
+@endpoints.api(name='itineraryMaker', version='v1',
+               audiences=[ANDROID_AUDIENCE],
+               allowed_client_ids=[WEB_CLIENT_ID, ANDROID_CLIENT_ID,
+                                   IOS_CLIENT_ID, API_EXPLORER_CLIENT_ID])
+class ItineraryMakerApi(remote.Service):
+    """Itinerary Maker Api Server"""
+
+    def _getUser(self):
+        """Creates new user"""
+        current_user = endpoints.get_current_user()
+        if not current_user:
+            raise endpoints.UnauthorizedException('Authorization Required')
+        user = User.query(User.email == current_user.email()).get()
+        if not user:
+            user = User.new_user(current_user.nickname(), current_user.email())
+        return user
+
+
+    @endpoints.method(request_message=UserForm,
+                      response_message=UserForm,
+                      path='user/update',
+                      name='update_user',
+                      http_method='POST')
+    def updateUser(self, request):
+        """Updates an existing username"""
+        user = self._getUser()
+        return user.update_user(request).to_form()
+
+
+    @endpoints.method(request_message=ChecklistForm,
+                      response_message=ChecklistForm,
+                      path='list',
+                      name='createList',
+                      http_method='POST')
+    def createList(self, request):
+        """Creates new list"""
+        user = self._getUser()
+        request.creator = user.email
+        checklist = Checklist.new_list(request)
+        return checklist.to_form()
+
+    @endpoints.method(request_message=UPDATE_LIST_REQUEST,
+                      response_message=ChecklistForm,
+                      path='list/{urlsafe_list_key}',
+                      name='updateList',
+                      http_method='PUT')
+    def updateList(self, request):
+        """Updates an existing list"""
+        user = self._getUser()
+        checklist = ndb.Key(urlsafe=request.urlsafe_list_key).get()
+        return checklist.update_list(request).to_form()
+
+    @endpoints.method(request_message=ItineraryForm,
+                      response_message=ItineraryForm,
+                      path='itinerary',
+                      name='createItinerary',
+                      http_method='POST')
+    def createItinerary(self, request):
+        """Creates new itinerary"""
+        user = self._getUser()
+        request.creator = user.email
+        itinerary = Itinerary.new_itinerary(request)
+        return itinerary.to_form()
+
+    @endpoints.method(request_message=UPDATE_ITIN_REQUEST,
+                      response_message=ItineraryForm,
+                      path='itinerary/{urlsafe_itin_key}',
+                      name='updateItinerary',
+                      http_method='PUT')
+    def updateItinerary(self, request):
+        """Updates an existing itinerary"""
+        user = self._getUser()
+        itinerary = ndb.Key(urlsafe=request.urlsafe_itin_key).get()
+        return itinerary.update_itinerary(request).to_form()
+
+    @endpoints.method(request_message=TransportForm,
+                      response_message=TransportForm,
+                      path='transport',
+                      name='createTransport',
+                      http_method='POST')
+    def createTransport(self, request):
+        """Creates new transport"""
+        user = self._getUser()
+        request.creator = user.email
+        transport = TransportItem.new_transport(request)
+        return transport.to_form()
+
+    @endpoints.method(request_message=UPDATE_TRAN_REQUEST,
+                      response_message=TransportForm,
+                      path='transport/{urlsafe_tran_key}',
+                      name='updateTransport',
+                      http_method='PUT')
+    def updateTransport(self, request):
+        """Updates an existing transport"""
+        user = self._getUser()
+        transport = ndb.Key(urlsafe=request.urlsafe_tran_key).get()
+        return transport.update_transport(request).to_form()
+
+api = endpoints.api_server([ItineraryMakerApi])
--- models.py
+++ models.py
@@ -15,8 +15,23 @@ from google.appengine.ext import ndb
 
 class User(ndb.Model):
     """User profile object"""
-    username = ndb.StringProperty(required=True)
+    username = ndb.StringProperty(required=True, indexed=False)
     email = ndb.StringProperty(required=True)
+
+    @classmethod
+    def new_user(cls, username, email):
+        """Creates and returns a new user"""
+        u_key = ndb.Key(User, email)
+        user = User(username=username, email=email, key=u_key)
+        user.put()
+        return user
+
+    def update_user(self, form):
+        """Updates user object"""
+        if form.username:
+            self.username = form.username
+            self.put()
+        return self
 
     def to_form(self):
         """Returns a UserForm representation of the User"""
@@ -30,14 +45,57 @@ class Itinerary(ndb.Model):
     startDate = ndb.DateProperty()
     endDate = ndb.DateProperty()
     emailReminder = ndb.BooleanProperty(default=False)
+    public = ndb.BooleanProperty(default=False)
     transports = ndb.KeyProperty(repeated=True, kind='TransportItem', indexed=False)
     sharedLists = ndb.KeyProperty(repeated=True, kind='Checklist', indexed=False)
+
+    @classmethod
+    def new_itinerary(cls, form):
+        """Creates and returns a new itinerary"""
+        itinerary = Itinerary(name=form.name, creator=ndb.Key(User, form.creator))
+        return itinerary.update_itinerary(form)
+
+    def update_itinerary(self, form):
+        """updates an itinerary with new values"""
+        if form.name:
+            self.name = form.name
+        if form.startDate:
+            self.startDate = form.startDate
+        if form.endDate:
+            self.endDate = form.endDate
+        if form.emailReminder:
+            self.emailReminder = form.emailReminder
+        if form.public:
+            self.public = form.public
+        self.put()
+        return self
+
+    def update_lists(self, form):
+        """Updates an itinerary's lists with new values"""
+        if form.sharedLists:
+            new_lists = []
+            for li in form.sharedLists:
+                new = ndb.Key(urlsafe=li)
+                new_lists.append(new)
+            self.sharedLists = new_lists
+            self.put()
+
+    def update_transports(self, form):
+        """Updates an itinerary's transports with new values"""
+        if form.transports:
+            new_tp = []
+            for tp in form.transports:
+                new = ndb.Key(urlsafe=tp)
+                new_tp.append(new)
+            self.transports = new_tp
+            self.put()
 
     def to_form(self):
         """Returns a ItineraryForm representation of the Itinerary"""
         form = ItineraryForm(name=self.name,
                              creator=self.creator.id(),
-                             emailReminder=self.emailReminder)
+                             emailReminder=self.emailReminder,
+                             public=self.public)
         if self.startDate:
             form.startDate = str(self.startDate)
         if self.endDate:
@@ -46,6 +104,7 @@ class Itinerary(ndb.Model):
             form.transports = [t.key.urlsafe() for t in self.transports]
         if self.sharedLists:
             form.sharedLists = [s.key.urlsafe() for s in self.sharedLists]
+        form.urlsafe_key = self.key.urlsafe()
         return form
 
 class TransportItem(ndb.Model):
@@ -58,6 +117,30 @@ class TransportItem(ndb.Model):
     arrive = ndb.DateProperty(indexed=False)
     notes = ndb.StringProperty(indexed=False)
     template = ndb.BooleanProperty(required=True, default=False)
+
+    @classmethod
+    def new_transport(cls, form):
+        """Creates and returns a new transport item"""
+        transport = TransportItem(name=form.name, creator=ndb.Key(User, form.creator))
+        return transport.update_transport(form)
+
+    def update_transport(self, form):
+        """updates a transport item with new values"""
+        if form.name:
+            self.name = form.name
+        if form.seats:
+            self.seats = form.seats
+            self.seatsAvailable = form.seats
+        if form.depart:
+            self.depart = form.depart
+        if form.arrive:
+            self.arrive = form.arrive
+        if form.notes:
+            self.notes = form.notes
+        if form.template:
+            self.template = form.template
+        self.put()
+        return self
 
     def to_form(self):
         """Returns TransportForm representation of Transportation"""
@@ -72,26 +155,7 @@ class TransportItem(ndb.Model):
             form.arrive = str(self.arrive)
         if self.notes:
             form.notes = self.notes
-        return form
-
-class Checklist(ndb.Model):
-    """Checklist object"""
-    name = ndb.StringProperty(required=True)
-    creator = ndb.KeyProperty(required=True, kind='User')
-    items = ndb.KeyProperty(repeated=True, kind='ListItem', indexed=False)
-    template = ndb.BooleanProperty(required=True, default=False)
-
-    def to_form(self):
-        """Returns ChecklistForm representation of Checklist"""
-        form = ChecklistForm(name=self.name,
-                             creator=self.creator.id(),
-                             template=self.template)
-        if self.startDate:
-            form.startDate = str(self.startDate)
-        if self.endDate:
-            form.endDate = str(self.endDate)
-        if self.items:
-            form.items = [i.key.urlsafe() for i in self.items]
+        form.urlsafe_key = self.key.urlsafe()
         return form
 
 class ListItem(ndb.Model):
@@ -101,7 +165,6 @@ class ListItem(ndb.Model):
     image = ndb.StringProperty(indexed=False)
     due = ndb.DateProperty()
     check = ndb.IntegerProperty(choices=[0, 1, 2], default=0)
-
 
     def to_form(self):
         """Returns ListItemForm representation of ListItem"""
@@ -115,6 +178,58 @@ class ListItem(ndb.Model):
             form.due = str(self.due)
         return form
 
+class Checklist(ndb.Model):
+    """Checklist object"""
+    name = ndb.StringProperty(required=True)
+    creator = ndb.KeyProperty(required=True, kind='User')
+    items = ndb.StructuredProperty(ListItem, repeated=True)
+    template = ndb.BooleanProperty(default=False)
+
+    @classmethod
+    def new_list(cls, form):
+        """Creates and returns a new checklist"""
+        checklist = Checklist(name=form.name, creator=ndb.Key(User, form.creator))
+        return checklist.update_list(form)
+
+    def update_list(self, form):
+        """updates a checklist with new values"""
+        if form.name:
+            self.name = form.name
+        if form.items:
+            new_items = []
+            for item in form.items:
+                new = ListItem(name=item.name)
+                if item.notes:
+                    new.notes = item.notes
+                if item.image:
+                    new.image = item.image
+                if item.due:
+                    new.due = item.due
+                if item.check:
+                    new.check = item.check
+                new_items.append(new)
+            self.items = new_items
+        if form.template:
+            self.template = form.template
+        self.put()
+        return self
+
+    def to_form(self):
+        """Returns ChecklistForm representation of Checklist"""
+        form = ChecklistForm(name=self.name,
+                             creator=self.creator.id(),
+                             template=self.template)
+        if self.items:
+            form.items = [i.to_form() for i in self.items]
+        form.urlsafe_key = self.key.urlsafe()
+        return form
+
+        #if items:
+         #   for item in items:
+          #      checklist.items.append(ndb.Key(urlsafe=item).get())
+
+
+
 class UserForm(messages.Message):
     """UserForm for outbound form message"""
     username = messages.StringField(1)
@@ -123,31 +238,26 @@ class UserForm(messages.Message):
 class ItineraryForm(messages.Message):
     """ItineraryForm for outbound form message"""
     name = messages.StringField(1, required=True)
-    creator = messages.IntegerField(2, required=True)
+    creator = messages.StringField(2)
     startDate = messages.StringField(3)
     endDate = messages.StringField(4)
     emailReminder = messages.BooleanField(5)
-    transports = messages.StringField(6, repeated=True)
-    sharedLists = messages.StringField(7, repeated=True)
+    public = messages.BooleanField(6)
+    transports = messages.StringField(7, repeated=True)
+    sharedLists = messages.StringField(8, repeated=True)
+    urlsafe_key = messages.StringField(9)
 
 class TransportForm(messages.Message):
     """TransportForm for outbound form message"""
     name = messages.StringField(1, required=True)
-    creator = messages.IntegerField(2, required=True)
+    creator = messages.StringField(2)
     seats = messages.IntegerField(3)
     seatsAvailable = messages.IntegerField(4)
     depart = messages.StringField(5)
     arrive = messages.StringField(6)
     notes = messages.StringField(7)
     template = messages.BooleanField(8)
-
-
-class ChecklistForm(messages.Message):
-    """ChecklistForm for outbound form message"""
-    name = messages.StringField(1, required=True)
-    creator = messages.IntegerField(2, required=True)
-    items = messages.StringField(3, repeated=True)
-    template = messages.BooleanField(4)
+    urlsafe_key = messages.StringField(9)
 
 class ListItemForm(messages.Message):
     """ListItemForm for outbound form message"""
@@ -156,3 +266,15 @@ class ListItemForm(messages.Message):
     image = messages.StringField(3)
     due = messages.StringField(4)
     check = messages.IntegerField(5)
+
+class ChecklistForm(messages.Message):
+    """ChecklistForm for outbound form message"""
+    name = messages.StringField(1, required=True)
+    creator = messages.StringField(2)
+    items = messages.MessageField(ListItemForm, 3, repeated=True)
+    template = messages.BooleanField(4)
+    urlsafe_key = messages.StringField(5)
+
+class ListItemForms(messages.Message):
+    """ListItemForms for multiple outbound form messages"""
+    items = messages.MessageField(ListItemForm, 1, repeated=True)
